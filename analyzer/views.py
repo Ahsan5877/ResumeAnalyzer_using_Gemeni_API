fe8f674bc8f6aeaa6029718b1af1f1489449c553
Ahasn5877 <ahsaniqbal.aidev@gmail.com>
2025-07-07 15:40:48 +0500

    Remove Errors

--- analyzer/views.py
+++ analyzer/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Resume, AnalysisResume
 from .serializers import ResumeSerializer, AnalysisResumeSerializer
+from .utils import extract_text_from_file
 
 
 
